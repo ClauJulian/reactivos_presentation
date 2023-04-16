e9ad56b78185933b64843ebf6e4af91810a00437
ClaudiaJulian <claudia_e_julian@yahoo.com.ar>
2023-04-16 16:48:33 -0300

    comentarios

--- src/App.js
+++ src/App.js
@@ -8,11 +8,10 @@ import Footer from './components/Footer/Footer';
 
 import json from './utis/grupo2.json';
 
-// APP_JSX - Desestructurar el json y armar 3 Objetos -> leader, developers, grupo2.
-// APP_JSX - Crear con Objetos -> leader tiene que tener: rol, email, img
-//                   -> developers tiene que tener: rol, email, img
-//                   -> grupo2 tiene que tener: id, nombreGrupo
-// APP_JSX - Enviar las props para que los componentes las trabajen
+// APP_JSX - Desestructurar el json y armar 2 Objetos -> members, grupo2.
+//          -> members tiene que tener: id, rol, email, img
+//          -> grupo2 tiene que tener: id, nombreGrupo
+// APP_JSX - Enviar los objetos a los componentes
 // APP_JSX - Armar la estructura de Presentacion.
 // APP_CSS - Generar los estilos en App.css
 

5468eac8435b3453f7aff8ac85cb951009f292c6
Guillermo <zeduard89@gmail.com>
2023-04-17 02:13:12 -0300

    correcion leader

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-//import logo from './logo.svg';
+//import logo from './logo.svg';     //////////////////
 import './App.css';
 import Titulo from './components/Titulo/Titulo';
 import Leader from './components/Leader/Leader';

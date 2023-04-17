343db066dc7e687a771d877c0ac3bce8d40e2ec6
Cristian <cristian.zolkowski@gmail.com>
2023-04-17 17:57:25 -0300

    Developers

--- src/components/Developers/Developers.jsx
+++ src/components/Developers/Developers.jsx
@@ -2,19 +2,21 @@ import React from 'react'
 
 import styleDevelopers from './Developers.module.css'
 
-// 1. DEVELOPERS_JSX -> Traer el modelo de /Card
-// 2. DEVELOPERS_JSX -> Usar el Objeto "members" de APP y crear la vista de los 4 Developers. Se puede usar filter() para rescatar los developers.
-// 3. DEVELOPERS_MODULE.CSS -> Generar los estilos en Developers.module.css
-// 4. DEVELOPERS_JSX Usar -> los estilos con className={styleDevelopers.NOMBRECLASE}
+import Card from './Card';
 
 
-const Developers = (props) => {
-  
-  
-  
+const Developers = ({developers}) => {
+   
   return (
-    <div>Developers</div>
-  )
-}
+    <div className="grid-container">
+      {developers.map((member) => (
+        <div key={member.id} className="grid-item">
+          <Card member={member} />
+        </div>
+      ))}
+    </div>
+  );
+};
+
 
 export default Developers
